d0b0dbfd243a5aa8edbafc99561b998b75ae68cb
afrofarmr <oluwafewa.adegoke@gmail.com>
2022-10-20 15:47:52 +0800

    Initial commit

---
+++ src/MySubClass.java
@@ -0,0 +1,26 @@
+public class MySubClass extends MySuperClass{
+
+    String commonStr = "sub common string";
+    public MySubClass(String constructorStr){
+        super(constructorStr);
+        System.out.println("sub class constructor");
+    }
+
+    public void subClassMethod(){
+        System.out.println("sub class method");
+    }
+
+    public void printCommonString(){
+        System.out.println(commonStr);
+        System.out.println(super.commonStr);
+    }
+
+    public  void  commonMethod(){
+        System.out.println("sub common method");
+    }
+
+    public void printCommonMethod(){
+        commonMethod();
+        super.commonMethod();
+    }
+}
